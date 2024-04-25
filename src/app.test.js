100d6cd3bb39c2dcfeecebbd53afaaccb5677283
LAPTOP-3BO2RVT9\acer120 <point640@gmail.com>
2024-04-25 11:13:58 +0700

    update app.js and app.test.js

--- src/app.test.js
+++ src/app.test.js
@@ -1,7 +1,12 @@
-const { dayOfTheWeek } = require('./app');
+const { dayOfTheWeek } = require("./app");
+const { monthOfYear } = require("./app");
 
+test("getDay returns the long-format day of the week", () => {
+  const day = dayOfTheWeek(new Date("3/11/2020"));
+  expect(day).toBe("Wednesday");
+});
 
-test('getDay returns the long-format day of the week', () => {
-    const day = dayOfTheWeek( new Date('3/11/2020') );
-    expect( day ).toBe('Wednesday');
+test("getMonth returns the long-format month of the year", () => {
+  const month = monthOfYear(new Date("3/11/2020"));
+  expect(month).toBe("March");
 });
